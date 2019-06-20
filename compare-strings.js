d72180b83fab10d68aed807d3039e8f7d65ce648
Kesara Wimal <kesarawimal9@gmail.com>
2019-06-20 15:39:39 +0530

    another algorithm

--- compare-strings.js
+++ compare-strings.js
@@ -11,9 +11,34 @@ function compareStrings(first, second) {
     if (first.length === 1 && second.length === 1) return 0;         // both are 1-letter strings
     if (first.length < 2 || second.length < 2) return 0;			 // if either is a 1-letter string
 
-    // return (coefficient(first, second) + distance(first, second)) / 2;
+    return (coefficient(first, second) + distance(first, second) + stringSimilarity(first, second)) / 3;
     // return coefficient(first, second);
-    return distance(first, second);
+    // return distance(first, second);
+    // return stringSimilarity(first, second);
+}
+
+function stringSimilarity(str1, str2) {
+    let substringLength = 2;
+    if (str1.length < substringLength || str2.length < substringLength)
+        return 0;
+
+    const map = new Map();
+    for (let i = 0; i < str1.length - (substringLength - 1); i++) {
+        const substr1 = str1.substr(i, substringLength);
+        map.set(substr1, map.has(substr1) ? map.get(substr1) + 1 : 1);
+    }
+
+    let match = 0;
+    for (let j = 0; j < str2.length - (substringLength - 1); j++) {
+        const substr2 = str2.substr(j, substringLength);
+        const count = map.has(substr2) ? map.get(substr2) : 0;
+        if (count > 0) {
+            map.set(substr2, count - 1);
+            match++;
+        }
+    }
+
+    return (match * 2) / (str1.length + str2.length - ((substringLength - 1) * 2));
 }
 
 function coefficient(first, second) {
